de963f83479d4ed297d008512e82d4fd1e5d1a51
ROBERT ANGEL TORRES <rtorres61595@gmail.com>
2020-06-03 20:35:12 -0400

    done

--- app.js
+++ app.js
@@ -10,110 +10,234 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 const teamMembers = [];
-function managerReq(){
-    inquirer.prompt( [
+function start() {
+  function createManager() {
+    // prompt user for manager information
+    // use answers to create manager and add to the team members array
+    // when done call create team function
+    inquirer
+      .prompt([
         {
+          type: "input",
+          name: "name",
+          message: "What is your name?",
+          validate: (answer) => {
+            if (answer !== "") {
+              return true;
+            }
+            return "Please enter at least one character.";
+          },
+        },
+        {
+          type: "input",
+          name: "id",
+          message: "What is your ID?",
+          validate: (answer) => {
+            if (answer !== "") {
+              return true;
+            }
+            return "Please enter at least one character.";
+          },
+        },
+        {
+          type: "input",
+          name: "email",
+          message: "what is your email?",
+          validate: (answer) => {
+            if (answer !== "") {
+              return true;
+            }
+            return "Please enter at least one character.";
+          },
+        },
+        {
+          type: "input",
+          name: "officeNumber",
+          message: "What is your office number?",
+          validate: (answer) => {
+            if (answer !== "") {
+              return true;
+            }
+            return "Please enter at least one character.";
+          },
+        },
+      ])
+      .then(function (answers) {
+        const manager = new Manager(
+          answers.name,
+          answers.id,
+          answers.email,
+          answers.officeNumber
+        );
+        teamMembers.push(manager);
+        console.log(teamMembers);
+        createTeam();
+      });
+  }
+
+  function createTeam() {
+    inquirer
+      .prompt([
+        {
+          type: "list",
+          name: "teamRole",
+          message: "What type of employee would you like to add?",
+          choices: ["Engineer", "Intern", "done"],
+        },
+      ])
+      .then(function (answers) {
+        switch (answers.teamRole) {
+          case "Engineer":
+            addEngineer();
+            break;
+          case "Intern":
+            addIntern();
+            break;
+          default:
+            buildTeam();
+        }
+      });
+  }
+
+  function addEngineer() {
+    //use inquirer to ask questions for engineer
+    // use the answers to create a engineer and add to the teamMembers array
+    //when done call the create team function
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "name",
+          message: "What is your name?",
+          validate: (answer) => {
+            if (answer !== "") {
+              return true;
+            }
+            return "Please enter at least one character.";
+          },
+        },
+        {
+          type: "input",
+          name: "id",
+          message: "What is your ID?",
+          validate: (answer) => {
+            if (answer !== "") {
+              return true;
+            }
+            return "Please enter at least one character.";
+          },
+        },
+        {
+          type: "input",
+          name: "email",
+          message: "what is your email?",
+          validate: (answer) => {
+            if (answer !== "") {
+              return true;
+            }
+            return "Please enter at least one character.";
+          },
+        },
+        {
+          type: "input",
+          name: "github",
+          message: "What is your github user name?",
+          validate: (answer) => {
+            if (answer !== "") {
+              return true;
+            }
+            return "Please enter at least one character.";
+          },
+        },
+      ])
+    .then(function (answers) {
+      const engineer = new Engineer(
+        answers.name,
+        answers.id,
+        answers.email,
+        answers.github
+      );
+      teamMembers.push(engineer);
+      console.log(teamMembers);
+      createTeam();
+    });
+   }
+
+  function addIntern() {
+    //use inquirer to ask questions for Intern
+    // use the answers to create a intern and add to the teamMembers array
+    //when done call the create team function
+    inquirer
+    .prompt([
+      {
         type: "input",
         name: "name",
         message: "What is your name?",
-        validate: answer => {
-            if (answer !== "") {
-              return true;
-            }
-            return "Please enter at least one character.";
+        validate: (answer) => {
+          if (answer !== "") {
+            return true;
           }
-    
+          return "Please enter at least one character.";
         },
-        {
+      },
+      {
         type: "input",
         name: "id",
         message: "What is your ID?",
-        validate: answer => {
-            if (answer !== "") {
-              return true;
-            }
-            return "Please enter at least one character.";
+        validate: (answer) => {
+          if (answer !== "") {
+            return true;
           }
-    
+          return "Please enter at least one character.";
         },
-        {
+      },
+      {
         type: "input",
         name: "email",
         message: "what is your email?",
-        validate: answer => {
-            if (answer !== "") {
-              return true;
-            }
-            return "Please enter at least one character.";
+        validate: (answer) => {
+          if (answer !== "") {
+            return true;
           }
-    
+          return "Please enter at least one character.";
         },
-        {
+      },
+      {
         type: "input",
-        name: "officeNumber",
-        message: "What is your office number?",
-        validate: answer => {
-            if (answer !== "") {
-              return true;
-            }
-            return "Please enter at least one character.";
+        name: "school",
+        message: "What is your github user name?",
+        validate: (answer) => {
+          if (answer !== "") {
+            return true;
           }
-    
+          return "Please enter at least one character.";
         },
-    
-        ]).then(function(answers){
-           const manager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
-           teamMembers.push(manager)
-           console.log(teamMembers)
-            createTeam()
-        })
+      },
+    ])
+  .then(function (answers) {
+    const intern = new Intern(
+      answers.name,
+      answers.id,
+      answers.email,
+      answers.school
+    );
+    teamMembers.push(intern);
+    console.log(teamMembers);
+    createTeam();
+  });
+ }
+  
+
+  function buildTeam() {
+    if (!fs.existsSync(OUTPUT_DIR)) {
+      fs.mkdirSync(OUTPUT_DIR);
     }
+    fs.writeFileSync(outputPath, render(teamMembers), "utf-8");
+  }
 
-    function createTeam() {
-        inquirer.prompt([{
-            type: "list",
-        name: "teamRole",
-        message: "What type of employee would you like to add?",
-         choices: ["Engineer", "Intern", "done"]
+  createManager();
+
+}
+  start();
 
 
-        }]) .then(function (answers){
-
-            switch(answers.teamRole) {
-                case "Engineer":
-                 addEngineer() 
-                  break;
-                case "Intern":
-                    addIntern()
-           
-                  break;
-                default:"done"
-                  // code block
-              }   
-        })
-    }
-          
-    managerReq()
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
